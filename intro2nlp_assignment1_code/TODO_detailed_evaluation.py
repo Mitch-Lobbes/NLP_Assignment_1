5f9eb1ec355a1ec3e45e9141f99c875fd0ac516a
unknown <p.s.feil@student.vu.nl>
2022-04-17 12:27:02 +0200

    Easter Sunday Update

--- intro2nlp_assignment1_code/TODO_detailed_evaluation.py
+++ intro2nlp_assignment1_code/TODO_detailed_evaluation.py
@@ -3,6 +3,7 @@
 
 import pandas as pd
 from sklearn.metrics import f1_score, precision_recall_fscore_support
+from sklearn.metrics import f1_score
 
 results = pd.read_csv(filepath_or_buffer='D:\\Users\\Paola\\Documents\\University\\Master\\NLP\\Assignment_1\\NLP_Assignment_1\\intro2nlp_assignment1_code\\experiments\\base_model\\model_output.tsv', encoding='latin-1', names=["Word", "Gold", "Prediction"], sep="\t")
 
@@ -15,5 +16,5 @@ C_metrics = precision_recall_fscore_support(results["Gold"], results["Prediction
 N_metrics = precision_recall_fscore_support(results["Gold"], results["Prediction"], average='binary', pos_label='N')
 overall = precision_recall_fscore_support(results["Gold"], results["Prediction"], average='weighted')
 print("\nEvaluation metrics for class C:\n---------------------------------------------------------------------\n", C_metrics)
-print("\nEvaluation metrics for class C:\n---------------------------------------------------------------------\n", N_metrics)
-print("\nWeighted average F1 score:\n---------------------------------------------------------------------\n", overall)
+print("\nEvaluation metrics for class N:\n---------------------------------------------------------------------\n", N_metrics)
+print("\nWeighted average F1 score:", overall[2])
