b78384781cf7b00ec3b3f504b1854699ed5d626e
lclass <88320133+lclass@users.noreply.github.com>
2022-04-19 16:46:50 +0200

    table done but baseline predictions need to be saved correctly

--- intro2nlp_assignment1_code/TODO_detailed_evaluation.py
+++ intro2nlp_assignment1_code/TODO_detailed_evaluation.py
@@ -7,48 +7,49 @@ from sklearn.metrics import f1_score
 import json
 import os
 
-results = pd.read_csv(filepath_or_buffer='D:\\Users\\Paola\\Documents\\University\\Master\\NLP\\Assignment_1\\NLP_Assignment_1\\intro2nlp_assignment1_code\\experiments\\base_model\\model_output.tsv', encoding='latin-1', names=["Word", "Gold", "Prediction"], sep="\t")
 
-print(results) #data contains some useless rows
+class Detailed_Eval:
 
-results = results.dropna(axis=0)
-print(results)
+    def __init__(self, filelist):
+        self.filelist = filelist
+        self.model_list = ["Random", "Majority", "Length", "Frequency", "LSTM"]
+        self.table = pd.DataFrame(columns=["Model",
+                                           "Class_N Precision",
+                                           "Class_N Recall",
+                                           "Clgit ass_N F1",
+                                           "Class_C Precision",
+                                           "Class_C Recall",
+                                           "Class_C F1",
+                                           "Weighted Average F1"])
+        self.update_table()
+        self.print_table()
 
-C_metrics = precision_recall_fscore_support(results["Gold"], results["Prediction"], average='binary', pos_label='C')
-N_metrics = precision_recall_fscore_support(results["Gold"], results["Prediction"], average='binary', pos_label='N')
-overall = precision_recall_fscore_support(results["Gold"], results["Prediction"], average='weighted')
-print("\nEvaluation metrics for class C:\n---------------------------------------------------------------------\n", C_metrics)
-print("\nEvaluation metrics for class N:\n---------------------------------------------------------------------\n", N_metrics)
-print("\nWeighted average F1 score:", overall[2])
+    def update_table(self):
+        for filepath in enumerate(self.filelist):
+            file_output = pd.read_csv(filepath_or_buffer=filepath[1], encoding='latin-1',
+                                      names=["Word", "Gold", "Prediction"],
+                                      sep="\t")
+            file_output = file_output.dropna(axis=0)
+
+            C_metrics = precision_recall_fscore_support(file_output["Gold"], file_output["Prediction"],
+                                                        average='binary', pos_label='C')
+            N_metrics = precision_recall_fscore_support(file_output["Gold"], file_output["Prediction"],
+                                                        average='binary', pos_label='N')
+            weighted_avg_f1 = precision_recall_fscore_support(file_output["Gold"], file_output["Prediction"],
+                                                              average='weighted')
+            self.table.loc[len(self.table.index)] = [self.model_list[filepath[0]], round(N_metrics[0], 2),
+                                                     round(N_metrics[1], 2), round(N_metrics[2], 2),
+                                                     round(C_metrics[0], 2), round(C_metrics[1], 2),
+                                                     round(C_metrics[2], 2), round(weighted_avg_f1[0], 2)]
+
+    def print_table(self):
+        print("Detailed Evaluation (2.5 points)")
+        print(self.table.to_string())
+        print("\n")
 
 
-#Save different F1 values for question 14
+# !!! The files need to be in the correct order --> "Random", "Majority", "Length", "Frequency", "LSTM" !!!
 
-f = open('D:\\Users\\Paola\\Documents\\University\\Master\\NLP\Assignment_1\\NLP_Assignment_1\\intro2nlp_assignment1_code\\experiments\\base_model\\params.json')
-params = json.load(f)
-epochs = params["num_epochs"]
-print(f"NN ran with {epochs} epochs.") 
-f.close()
-
-f_path = 'D:\\Users\\Paola\\Documents\\University\\Master\\NLP\Assignment_1\\NLP_Assignment_1\\intro2nlp_assignment1_code\\experiments\\base_model\\F1Values.json'
-
-with open(f_path, "a+") as d:
-    
-    # check if size of file is 0
-    if os.path.getsize(f_path) == 0:
-        print('File is empty')
-
-        fdict = {}
-        fdict[epochs] = overall[2]
-        json.dump(fdict, d)
-    
-    else:
-        print('File is not empty')
-
-        with open(f_path, "r+") as z:
-            fdict = json.load(z)
-            if str(epochs) not in fdict:
-                fdict.update({epochs:overall[2]})
-        with open(f_path, "w") as q:    
-            json.dump(fdict,q)
+lstm_output = ['experiments/base_model/model_output.tsv']
+ex12 = Detailed_Eval(lstm_output)
 
